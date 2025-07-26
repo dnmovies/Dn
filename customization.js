db6553cba54b9f4b3fb17bcbe30f4277f2caf4ec
dnmovies <dnmovies6@gmail.com>
2025-07-26 15:00:44 +0530

    Update customization.js

--- customization.js
+++ customization.js
@@ -5,51 +5,71 @@ module.exports = {
   ARTISTS:[
       "Ranwan Liyanage original",
       "Amal Perera original",
+      "Mankkalpitha original",
+      "Momo original",
+      "Sangeeth original",
+      "Nonstop original",
       "Clarence Wijewardena original",
       "Sunil Edirisinghe original",
       "Victor Ratnayake original",
       "Nanda Malini original",
       "Sanuka Wickramasinghe original",
+      "manakkalpitha original",
+      "chamara ranawaka original",
       "Kasun Kalhara original",
       "Uresha Ravihari original",
+      "damith asnka original",
+      "jothipala original",
+      "raps original",
       "Dinesh Kanagaratnam original"
     ],
   //====================menu=================================
   MENUMSG: function (pushname, runtime, config) {
     return `
-✘◍ ꜱᴇʟᴇᴛᴇ ʏᴏᴜʀ ᴀᴅᴠᴇɴᴛᴜʀᴛ. 
-ᴛʜᴀɴᴋꜱ ꜰᴏʀ ᴜꜱɪɴɢ ʙᴏᴛ.
+✘◍ ᴛʜᴀɴᴋꜱ ꜰᴏʀ ᴜꜱɪɴɢ ʙᴏᴛ.
 
 ┏━━━━❮ 📆 ᴛᴏ ᴅᴀʏ 📆❯━━━━
 ┃
 ┃ 📅 Date Today : ${new Date().toLocaleDateString("en-GB", {
       timeZone: "Asia/Colombo",
     })}
-┃ ⌚ Time Now : ${new Date().toLocaleTimeString("en-GB", {
+┃ ⏰ Time Now : ${new Date().toLocaleTimeString("en-GB", {
       timeZone: "Asia/Colombo",
     })}
 ┃
 ┗━━━━━━━━━━━━━━━
 ┏━━━━❮📝 ᴅᴇᴛᴇʟᴇꜱ 📝❯━━━
-┃🗣️ 𝚄𝚜𝚎𝚛 : ${pushname}
-┃🤖 𝙱𝚘𝚝 : © 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ V${packageJson.version}
-┃📜 𝙿𝚛𝚎𝚏𝚒𝚡 : ${config.PREFIX}
-┃📚 𝚅𝚎𝚛𝚜𝚒𝚘𝚗 : ${packageJson.version}
-┃📝 𝙿𝚕𝚊𝚝𝚏𝚘𝚛𝚖 : ${os.platform()}
-┃📟 𝙷𝚘𝚜𝚝 : ${os.hostname()}
-┃🤴𝙾𝚠𝚗𝚎𝚛 : ${config.OWNER_NAME}
+┃☠️ 𝚄𝚜𝚎𝚛 : ${pushname}
+┃🎃 𝙱𝚘𝚝 : 𝙳𝙸𝙻𝙰 V${packageJson.version}
+┃🧩 𝙿𝚛𝚎𝚏𝚒𝚡 : ${config.PREFIX}
+┃🎡 𝚅𝚎𝚛𝚜𝚒𝚘𝚗 : ${packageJson.version}
+┃📡 𝙿𝚕𝚊𝚝𝚏𝚘𝚛𝚖 : ${os.platform()}
+┃💡 𝙷𝚘𝚜𝚝 : ${os.hostname()}
+┃🧸 𝙾𝚠𝚗𝚎𝚛 :DILAKSHA PRABATH
 ┃🔊 𝙼𝚘𝚍𝚎 : ${config.MODE}
-┃🍁 𝚄𝚙𝚝𝚒𝚖𝚎 : ${runtime(process.uptime())}
-┃✨𝙼𝚎𝚖 : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(
+┃🧮 𝚄𝚙𝚝𝚒𝚖𝚎 : ${runtime(process.uptime())}
+┃🪅 𝙼𝚎𝚖 : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(
       2
     )}MB / ${Math.round(os.totalmem() / 1024 / 1024)}MB
 ┗━━━━━━━━━━━━━━━
+*╔⏤⏤⏤⏤⏤⏤╝ᎴᏁ ╚⏤⏤⏤⏤⏤⏤╗*
+
+⭕ *ΝᎬᏔՏ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAyHEQ7tkjI6v4ACw30
+⭕ *ᎷᏆႮՏᏆᏟ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029Vb3uqklAInPchBUjck3l
+⭕ *ᎷϴᏙᏆᎬ ႮᏢᎠᎪͲᎬ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAXPal4yltU1ep32l0H
+⭕ *ՏͲᏆᏦᎬᎡ ᏔϴᎡᏞᎠ ᏟᎻᎪΝΝᎬᏞ ➻*
+https://whatsapp.com/channel/0029Vb5xbu6JuyAAH9biN10H
+
+*╚⏤⏤⏤⏤⏤⏤╗ᎴᏁ ╔⏤⏤⏤⏤⏤⏤╝*
         `.trim();
   },
   //======================download============================
   TIKTOK: function (title, author, q) {
     return `
-🎟️ *QUEEN ANJU-MD TIKTOK DOWNLOADER* 🎟️
+🎀 *𝑫𝑰𝑳𝑨 TIKTOK DOWNLOADER* 🎀
 
 🔢 *Please reply with the number you want to select:*
 
@@ -60,7 +80,7 @@ URL    * ${q}
   },
   FACEBOOK: function (title, q) {
     return `
-💢 QUEEN ANJU-MD FB DOWNLOADER 💢
+❄️ 𝑫𝑰𝑳𝑨 FB DOWNLOADER ❄️
     
 🎞 TITLE 🎞 ${title}
 
@@ -69,7 +89,7 @@ Fb-Url: -=-${q}
   },
   TWITTER: function (desc, q) {
     return `
-💢 QUEEN ANJU-MD TWITTER DOWNLOADER 💢
+🪼 𝑫𝑰𝑳𝑨 TWITTER DOWNLOADER 🪼
 
 📝 Description: ${desc || "No description"}
 
@@ -77,11 +97,11 @@ Twitter URL: ${q}
         `.trim();
   },
   //=================main==================================
-  ALIVEIMG: "./src/media/LOGOS/alive.jpg",
+  ALIVEIMG: "https://i.ibb.co/WNw4s8dY/Flux-Dev-A-hyperdetailed-fantasy-poster-of-a-beautiful-young-w-3.jpg",
   ALIVEVOICE: "./src/media/Auto_voice/alive.aac",
   ALIVEMSG: function (hostname, config, pushname, runtime) {
     return `
-🧚‍♂️⃟🩵 𝐐𝐔𝐄𝐄𝐍 𝐀𝐍𝐉𝐔 𝐀𝐋𝐈𝐕𝐄 𝐒𝐓𝐀𝐓𝐔𝐒 🧚‍♂️⃟🩵
+🐤 𝑫𝑰𝑳𝑨 𝐀𝐋𝐈𝐕𝐄 𝐒𝐓𝐀𝐓𝐔𝐒 🐤
 
 ✘◍ 𝗜'𝗺 𝗔𝗹𝗶𝘃𝗲, 𝗧𝗵𝗮𝗻𝗸𝘀 𝗳𝗼𝗿 𝗔𝘀𝗸𝗶𝗻𝗴!
 
@@ -90,26 +110,39 @@ Twitter URL: ${q}
 ┃ 📅 Date Today: ${new Date().toLocaleDateString("en-GB", {
       timeZone: "Asia/Colombo",
     })}
-┃ ⌚ Time Now: ${new Date().toLocaleTimeString("en-GB", {
+┃ ⏰ Time Now: ${new Date().toLocaleTimeString("en-GB", {
       timeZone: "Asia/Colombo",
     })}
 ┃
 ┗━━━━━━━━━━━━━━━
 ┏━━━━❮📝 𝗦𝘁𝗮𝘁𝘂𝘀 𝗗𝗲𝘁𝗮𝗶𝗹𝘀 📝❯━━━
-┃🗣️ 𝚄𝚜𝚎𝚛 : ${pushname}
-┃🤖 𝙱𝚘𝚝 : © 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ V${packageJson.version}
-┃📜 𝙿𝚛𝚎𝚏𝚒𝚡 : ${config.PREFIX}
-┃📚 𝚅𝚎𝚛𝚜𝚒𝚘𝚗 : ${packageJson.version}
-┃📝 𝙿𝚕𝚊𝚝𝚏𝚘𝚛𝚖 : ${hostname}
-┃📟 𝙷𝚘𝚜𝚝 : ${os.hostname()}
-┃⚙️ 𝙼𝚘𝚍𝚎 : ${config.MODE}
-┃💻 𝚄𝚙𝚝𝚒𝚖𝚎 : ${runtime(process.uptime())}
-┃✨𝙼𝚎𝚖 : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(
+┃☠️ 𝚄𝚜𝚎𝚛 : ${pushname}
+┃🎃 𝙱𝚘𝚝 : 𝑫𝑰𝑳𝑨 V${packageJson.version}
+┃🧩 𝙿𝚛𝚎𝚏𝚒𝚡 : ${config.PREFIX}
+┃🎡 𝚅𝚎𝚛𝚜𝚒𝚘𝚗 : ${packageJson.version}
+┃📡 𝙿𝚕𝚊𝚝𝚏𝚘𝚛𝚖 : ${hostname}
+┃💡𝙷𝚘𝚜𝚝 : ${os.hostname()}
+┃🔊 𝙼𝚘𝚍𝚎 : ${config.MODE}
+┃🧮 𝚄𝚙𝚝𝚒𝚖𝚎 : ${runtime(process.uptime())}
+┃🪅 𝙼𝚎𝚖 : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(
       2
     )}MB / ${Math.round(require("os").totalmem / 1024 / 1024)}MB
 ┗━━━━━━━━━━━━━━━
 
-💬 ᴇɴᴏʏ ᴛʜᴇ 𝗯𝗼𝘁 ᴏ𝗳 𝚚𝚞𝚎𝚎𝚗 𝗮𝗻𝗷𝘂! ✨
+*╔⏤⏤⏤⏤⏤⏤╝ᎴᏁ ╚⏤⏤⏤⏤⏤⏤╗*
+
+⭕ *ΝᎬᏔՏ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAyHEQ7tkjI6v4ACw30
+⭕ *ᎷᏆႮՏᏆᏟ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029Vb3uqklAInPchBUjck3l
+⭕ *ᎷϴᏙᏆᎬ ႮᏢᎠᎪͲᎬ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAXPal4yltU1ep32l0H
+⭕ *ՏͲᏆᏦᎬᎡ ᏔϴᎡᏞᎠ ᏟᎻᎪΝΝᎬᏞ ➻*
+https://whatsapp.com/channel/0029Vb5xbu6JuyAAH9biN10H
+
+*╚⏤⏤⏤⏤⏤⏤╗ᎴᏁ ╔⏤⏤⏤⏤⏤⏤╝*
+
+🧸 ᴇɴᴏʏ ᴛʜᴇ 𝗯𝗼𝘁 ᴏ𝗳 𝑫𝑰𝑳𝑨🫟
         `.trim();
   },
   //====================movie======================================
@@ -121,154 +154,323 @@ Twitter URL: ${q}
   },
   SINHALASUB: function (movieData) {
     return `
-🎬 *『 𝑨𝑵𝑱𝑼 Xᴾᴿᴼ 𝑪𝑰𝑵𝑬𝑴𝑨 』* 🍿
+📽️ *『 𝐃𝐍 𝐌𝐎𝐕𝐈𝐄𝐒 』* 🍿
 
-✨ *𝙼𝚘𝚟𝚒𝚎 𝙽𝚊𝚖𝚎:* ${movieData.title}
-⭐ *𝙸𝙼𝙳𝙱 𝚁𝚊𝚝𝚒𝚗𝚐:* ${movieData.rating?.value || "𝙽/𝙰"} ★
+🔮 *𝙼𝚘𝚟𝚒𝚎 𝙽𝚊𝚖𝚎:* ${movieData.title}
+🪄 *𝙸𝙼𝙳𝙱 𝚁𝚊𝚝𝚒𝚗𝚐:* ${movieData.rating?.value || "𝙽/𝙰"} ★
 📆 *𝚁𝚎𝚕𝚎𝚊𝚜𝚎 𝙳𝚊𝚝𝚎:* ${movieData.metadata?.releaseDate || "𝙽/𝙰"}
-🌐 *𝙲𝚘𝚞𝚗𝚝𝚛𝚢:* ${movieData.metadata?.country || "𝙽/𝙰"}
-⏱️ *𝙳𝚞𝚛𝚊𝚝𝚒𝚘𝚗:* ${movieData.metadata?.runtime || "𝙽/𝙰"}
+🌍 *𝙲𝚘𝚞𝚗𝚝𝚛𝚢:* ${movieData.metadata?.country || "𝙽/𝙰"}
+⏰ *𝙳𝚞𝚛𝚊𝚝𝚒𝚘𝚗:* ${movieData.metadata?.runtime || "𝙽/𝙰"}
+
+*╔⏤⏤⏤⏤⏤⏤╝ᎴᏁ ╚⏤⏤⏤⏤⏤⏤╗*
+
+⭕ *ΝᎬᏔՏ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAyHEQ7tkjI6v4ACw30
+⭕ *ᎷᏆႮՏᏆᏟ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029Vb3uqklAInPchBUjck3l
+⭕ *ᎷϴᏙᏆᎬ ႮᏢᎠᎪͲᎬ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAXPal4yltU1ep32l0H
+⭕ *ՏͲᏆᏦᎬᎡ ᏔϴᎡᏞᎠ ᏟᎻᎪΝΝᎬᏞ ➻*
+https://whatsapp.com/channel/0029Vb5xbu6JuyAAH9biN10H
+
+*╚⏤⏤⏤⏤⏤⏤╗ᎴᏁ ╔⏤⏤⏤⏤⏤⏤╝*
         `.trim();
   },
   CINESUBZ: function (title, metadata, rating) {
     return `
-🎬 *『 𝑨𝑵𝑱𝑼 Xᴾᴿᴼ 𝑪𝑰𝑵𝑬𝑴𝑨 』* 🍿
+📽️ *『 𝐃𝐍 𝐌𝐎𝐕𝐈𝐄𝐒 』* 🍿
 
-✨ *𝙼𝚘𝚟𝚒𝚎 𝙽𝚊𝚖𝚎:* 𝘾𝙝𝙚𝙘𝙠 𝙞𝙩 𝙤𝙪𝙩 → ${title}
+🔮 *𝙼𝚘𝚟𝚒𝚎 𝙽𝚊𝚖𝚎:* ${title}
        
-⭐ *𝙸𝙼𝙳𝙱 𝚁𝚊𝚝𝚒𝚗𝚐:* ${rating?.value || "𝙽/𝙰"} ★
+🪄 *𝙸𝙼𝙳𝙱 𝚁𝚊𝚝𝚒𝚗𝚐:* ${rating?.value || "𝙽/𝙰"} ★
 📆 *𝚁𝚎𝚕𝚎𝚊𝚜𝚎 𝙳𝚊𝚝𝚎:* ${metadata?.releaseDate || "𝙽/𝙰"}
-🌐 *𝙲𝚘𝚞𝚗𝚝𝚛𝚢:* ${metadata?.country || "𝙽/𝙰"}
-⏱️ *𝙳𝚞𝚛𝚊𝚝𝚒𝚘𝚗:* ${metadata?.runtime || "𝙽/𝙰"}
+🌍 *𝙲𝚘𝚞𝚗𝚝𝚛𝚢:* ${metadata?.country || "𝙽/𝙰"}
+⏰ *𝙳𝚞𝚛𝚊𝚝𝚒𝚘𝚗:* ${metadata?.runtime || "𝙽/𝙰"}
 🎭 *𝙶𝚎𝚗𝚛𝚎𝚜:* ${metadata?.genres.map((g) => `#${g}`).join(" • ") || "𝙽/𝙰"} 
+
+*╔⏤⏤⏤⏤⏤⏤╝ᎴᏁ ╚⏤⏤⏤⏤⏤⏤╗*
+
+⭕ *ΝᎬᏔՏ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAyHEQ7tkjI6v4ACw30
+⭕ *ᎷᏆႮՏᏆᏟ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029Vb3uqklAInPchBUjck3l
+⭕ *ᎷϴᏙᏆᎬ ႮᏢᎠᎪͲᎬ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAXPal4yltU1ep32l0H
+⭕ *ՏͲᏆᏦᎬᎡ ᏔϴᎡᏞᎠ ᏟᎻᎪΝΝᎬᏞ ➻*
+https://whatsapp.com/channel/0029Vb5xbu6JuyAAH9biN10H
+
+*╚⏤⏤⏤⏤⏤⏤╗ᎴᏁ ╔⏤⏤⏤⏤⏤⏤╝*
         `.trim();
   },
   CINETVSHOW: function (movieResponse) {
     return `
-🎬 *『 𝑨𝑵𝑱𝑼 Xᴾᴿᴼ 𝑪𝑰𝑵𝑬𝑴𝑨 』* 🍿
+📽️ *『 𝐃𝐍 𝐌𝐎𝐕𝐈𝐄𝐒 』* 🍿
 
 📺 *𝚃𝚅 𝚂𝚑𝚘𝚠:* ${movieResponse.episodeInfo?.title || "𝙽/𝙰"}
 🎞️ *𝙴𝚙𝚒𝚜𝚘𝚍𝚎 𝚃𝚒𝚝𝚕𝚎:* ${movieResponse.episodeInfo?.episodeTitle || "𝙽/𝙰"}
 📅 *𝙳𝚊𝚝𝚎:* ${movieResponse.episodeInfo?.date || "𝙽/𝙰"}
+
+*╔⏤⏤⏤⏤⏤⏤╝ᎴᏁ ╚⏤⏤⏤⏤⏤⏤╗*
+
+⭕ *ΝᎬᏔՏ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAyHEQ7tkjI6v4ACw30
+⭕ *ᎷᏆႮՏᏆᏟ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029Vb3uqklAInPchBUjck3l
+⭕ *ᎷϴᏙᏆᎬ ႮᏢᎠᎪͲᎬ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAXPal4yltU1ep32l0H
+⭕ *ՏͲᏆᏦᎬᎡ ᏔϴᎡᏞᎠ ᏟᎻᎪΝΝᎬᏞ ➻*
+https://whatsapp.com/channel/0029Vb5xbu6JuyAAH9biN10H
+
+*╚⏤⏤⏤⏤⏤⏤╗ᎴᏁ ╔⏤⏤⏤⏤⏤⏤╝*
         `.trim();
   },
   SINHALASUBTVSHOW: function (episodeInfo, quality) {
     return `
-🎬 *『 𝑨𝑵𝑱𝑼 Xᴾᴿᴼ 𝑪𝑰𝑵𝑬𝑴𝑨 』* 🍿
+📽️ *『 𝐃𝐍 𝐌𝐎𝐕𝐈𝐄𝐒 』* 🍿
 
 📺 *TV Show:* ${episodeInfo.title || "N/A"}
 🎞️ *Episode:* ${episodeInfo.episodeTitle || "N/A"}
 📅 *Date:* ${episodeInfo.date || "N/A"}
 💾 *Quality:* ${quality.toUpperCase()}
+
+*╔⏤⏤⏤⏤⏤⏤╝ᎴᏁ ╚⏤⏤⏤⏤⏤⏤╗*
+
+⭕ *ΝᎬᏔՏ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAyHEQ7tkjI6v4ACw30
+⭕ *ᎷᏆႮՏᏆᏟ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029Vb3uqklAInPchBUjck3l
+⭕ *ᎷϴᏙᏆᎬ ႮᏢᎠᎪͲᎬ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAXPal4yltU1ep32l0H
+⭕ *ՏͲᏆᏦᎬᎡ ᏔϴᎡᏞᎠ ᏟᎻᎪΝΝᎬᏞ ➻*
+https://whatsapp.com/channel/0029Vb5xbu6JuyAAH9biN10H
+
+*╚⏤⏤⏤⏤⏤⏤╗ᎴᏁ ╔⏤⏤⏤⏤⏤⏤╝*
         `.trim();
   },
   CINETVSHOWALLDL: function (movieData) {
     return `
-🎬 *『 𝑨𝑵𝑱𝑼 Xᴾᴿᴼ 𝑪𝑰𝑵𝑬𝑴𝑨 』* 🍿
+📽️ *『 𝐃𝐍 𝐌𝐎𝐕𝐈𝐄𝐒 』* 🍿
 
-✨ *𝙼𝚘𝚟𝚒𝚎 𝚃𝚒𝚝𝚕𝚎:* ${movieData.data.title}
+🔮 *𝙼𝚘𝚟𝚒𝚎 𝚃𝚒𝚝𝚕𝚎:* ${movieData.data.title}
 📆 *𝚁𝚎𝚕𝚎𝚊𝚜𝚎 𝙳𝚊𝚝𝚎:* ${movieData.data.releaseDate}
-🌐 *𝙽𝚎𝚝𝚠𝚘𝚛𝚔:* ${movieData.data.network}
+🌍 *𝙽𝚎𝚝𝚠𝚘𝚛𝚔:* ${movieData.data.network}
+
+*╔⏤⏤⏤⏤⏤⏤╝ᎴᏁ ╚⏤⏤⏤⏤⏤⏤╗*
+
+⭕ *ΝᎬᏔՏ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAyHEQ7tkjI6v4ACw30
+⭕ *ᎷᏆႮՏᏆᏟ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029Vb3uqklAInPchBUjck3l
+⭕ *ᎷϴᏙᏆᎬ ႮᏢᎠᎪͲᎬ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAXPal4yltU1ep32l0H
+⭕ *ՏͲᏆᏦᎬᎡ ᏔϴᎡᏞᎠ ᏟᎻᎪΝΝᎬᏞ ➻*
+https://whatsapp.com/channel/0029Vb5xbu6JuyAAH9biN10H
+
+*╚⏤⏤⏤⏤⏤⏤╗ᎴᏁ ╔⏤⏤⏤⏤⏤⏤╝*
         `.trim();
   },
   SINHALASUBTVSHOWALLDL: function (movieData) {
     return `
-🎬 *『 𝑨𝑵𝑱𝑼 Xᴾᴿᴼ 𝑪𝑰𝑵𝑬𝑴𝑨 』* 🍿
+📽️ *『 𝐃𝐍 𝐌𝐎𝐕𝐈𝐄𝐒 』* 🍿
 
 ✨ *𝙼𝚘𝚟𝚒𝚎 𝚃𝚒𝚝𝚕𝚎:* ${movieData.data.showInfo.title}
 📆 *𝚁𝚎𝚕𝚎𝚊𝚜𝚎 𝙳𝚊𝚝𝚎:* ${movieData.data.showInfo.releaseDate}
 🌐 *𝙽𝚎𝚝𝚠𝚘𝚛𝚔:* ${movieData.data.showInfo.network}
+
+*╔⏤⏤⏤⏤⏤⏤╝ᎴᏁ ╚⏤⏤⏤⏤⏤⏤╗*
+
+⭕ *ΝᎬᏔՏ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAyHEQ7tkjI6v4ACw30
+⭕ *ᎷᏆႮՏᏆᏟ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029Vb3uqklAInPchBUjck3l
+⭕ *ᎷϴᏙᏆᎬ ႮᏢᎠᎪͲᎬ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAXPal4yltU1ep32l0H
+⭕ *ՏͲᏆᏦᎬᎡ ᏔϴᎡᏞᎠ ᏟᎻᎪΝΝᎬᏞ ➻*
+https://whatsapp.com/channel/0029Vb5xbu6JuyAAH9biN10H
+
+*╚⏤⏤⏤⏤⏤⏤╗ᎴᏁ ╔⏤⏤⏤⏤⏤⏤╝*
         `.trim();
   },
   SIMNHALAMOVIE: function (title) {
     return `
-🎬 *『 𝑨𝑵𝑱𝑼 Xᴾᴿᴼ 𝑪𝑰𝑵𝑬𝑴𝑨 』* 🍿
+📽️ *『 𝐃𝐍 𝐌𝐎𝐕𝐈𝐄𝐒 』* 🍿
 
 🎞️ *𝚃𝚒𝚝𝚕𝚎:* ${title}  
+
+*╔⏤⏤⏤⏤⏤⏤╝ᎴᏁ ╚⏤⏤⏤⏤⏤⏤╗*
+
+⭕ *ΝᎬᏔՏ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAyHEQ7tkjI6v4ACw30
+⭕ *ᎷᏆႮՏᏆᏟ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029Vb3uqklAInPchBUjck3l
+⭕ *ᎷϴᏙᏆᎬ ႮᏢᎠᎪͲᎬ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAXPal4yltU1ep32l0H
+⭕ *ՏͲᏆᏦᎬᎡ ᏔϴᎡᏞᎠ ᏟᎻᎪΝΝᎬᏞ ➻*
+https://whatsapp.com/channel/0029Vb5xbu6JuyAAH9biN10H
+
+*╚⏤⏤⏤⏤⏤⏤╗ᎴᏁ ╔⏤⏤⏤⏤⏤⏤╝*
         `.trim();
   },
   AWAMOVIE: function (title, releaseDate, country) {
     return `
-🎬 *『 𝑨𝑵𝑱𝑼 Xᴾᴿᴼ 𝑪𝑰𝑵𝑬𝑴𝑨 』* 🍿
+📽️ *『 𝐃𝐍 𝐌𝐎𝐕𝐈𝐄𝐒 』* 🍿
 
-🎞️ *𝚃𝚒𝚝𝚕𝚎:* ${title}  
+🔮 *𝚃𝚒𝚝𝚕𝚎:* ${title}  
 📆 *𝚁𝚎𝚕𝚎𝚊𝚜𝚎 𝙳𝚊𝚝𝚎:* ${releaseDate}  
-🌐 *𝙲𝚘𝚞𝚗𝚝𝚛𝚢:*  ${country}      
+🌍 *𝙲𝚘𝚞𝚗𝚝𝚛𝚢:*  ${country}  
+
+ *╔⏤⏤⏤⏤⏤⏤╝ᎴᏁ ╚⏤⏤⏤⏤⏤⏤╗*
+
+⭕ *ΝᎬᏔՏ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAyHEQ7tkjI6v4ACw30
+⭕ *ᎷᏆႮՏᏆᏟ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029Vb3uqklAInPchBUjck3l
+⭕ *ᎷϴᏙᏆᎬ ႮᏢᎠᎪͲᎬ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAXPal4yltU1ep32l0H
+⭕ *ՏͲᏆᏦᎬᎡ ᏔϴᎡᏞᎠ ᏟᎻᎪΝΝᎬᏞ ➻*
+https://whatsapp.com/channel/0029Vb5xbu6JuyAAH9biN10H
+
+*╚⏤⏤⏤⏤⏤⏤╗ᎴᏁ ╔⏤⏤⏤⏤⏤⏤╝*
         `.trim();
   },
   ANIMEMOVIE: function (title, releaseDate, country, duration) {
     return `
-🎬 *『 𝑨𝑵𝑱𝑼 Xᴾᴿᴼ 𝑪𝑰𝑵𝑬𝑴𝑨 』* 🍿
+📽️ *『 𝐃𝐍 𝐌𝐎𝐕𝐈𝐄𝐒 』* 🍿
 
-✨ ${title} 
+🔮 ${title} 
 📅 𝗥𝗲𝗹𝗲𝗮𝘀𝗲 𝗗𝗮𝘁𝗲:${releaseDate}  
 🌍 𝗖𝗼𝘂𝗻𝘁𝗿𝘆:${country}  
-⏳ 𝗗𝘂𝗿𝗮𝘁𝗶𝗼𝗻:${duration}  
+⏰ 𝗗𝘂𝗿𝗮𝘁𝗶𝗼𝗻:${duration}
+
+*╔⏤⏤⏤⏤⏤⏤╝ᎴᏁ ╚⏤⏤⏤⏤⏤⏤╗*
+
+⭕ *ΝᎬᏔՏ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAyHEQ7tkjI6v4ACw30
+⭕ *ᎷᏆႮՏᏆᏟ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029Vb3uqklAInPchBUjck3l
+⭕ *ᎷϴᏙᏆᎬ ႮᏢᎠᎪͲᎬ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAXPal4yltU1ep32l0H
+⭕ *ՏͲᏆᏦᎬᎡ ᏔϴᎡᏞᎠ ᏟᎻᎪΝΝᎬᏞ ➻*
+https://whatsapp.com/channel/0029Vb5xbu6JuyAAH9biN10H
+
+*╚⏤⏤⏤⏤⏤⏤╗ᎴᏁ ╔⏤⏤⏤⏤⏤⏤╝*
         `.trim();
   },
   BAISCOMOVIE: function (title, year, rating, duration) {
     return `
-🎬 *『 𝑨𝑵𝑱𝑼 Xᴾᴿᴼ 𝑪𝑰𝑵𝑬𝑴𝑨 』* 🍿
+📽️ *『 𝐃𝐍 𝐌𝐎𝐕𝐈𝐄𝐒 』* 🍿
 
-🎞️ *𝚃𝚒𝚝𝚕𝚎:*  ${title} 
+🔮 *𝚃𝚒𝚝𝚕𝚎:*  ${title} 
 📅 𝗥𝗲𝗹𝗲𝗮𝘀𝗲 𝗗𝗮𝘁𝗲:${year}  
 🌍 RATINGS:${rating}  
-⏳ 𝗗𝘂𝗿𝗮𝘁𝗶𝗼𝗻:${duration}  
+⏰ 𝗗𝘂𝗿𝗮𝘁𝗶𝗼𝗻:${duration}  
+
+*╔⏤⏤⏤⏤⏤⏤╝ᎴᏁ ╚⏤⏤⏤⏤⏤⏤╗*
+
+⭕ *ΝᎬᏔՏ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAyHEQ7tkjI6v4ACw30
+⭕ *ᎷᏆႮՏᏆᏟ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029Vb3uqklAInPchBUjck3l
+⭕ *ᎷϴᏙᏆᎬ ႮᏢᎠᎪͲᎬ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAXPal4yltU1ep32l0H
+⭕ *ՏͲᏆᏦᎬᎡ ᏔϴᎡᏞᎠ ᏟᎻᎪΝΝᎬᏞ ➻*
+https://whatsapp.com/channel/0029Vb5xbu6JuyAAH9biN10H
+
+*╚⏤⏤⏤⏤⏤⏤╗ᎴᏁ ╔⏤⏤⏤⏤⏤⏤╝*
         `.trim();
   },
   ADULTMOVIE: function (title, rating, description) {
     return `
-🎬 *『 𝑨𝑵𝑱𝑼 Xᴾᴿᴼ 𝑪𝑰𝑵𝑬𝑴𝑨 』* 🍿
+📽️ *『 𝐃𝐍 𝐌𝐎𝐕𝐈𝐄𝐒 』* 🍿
 ──────────────────
-✨ *Title:* ${title}
-⭐ *Rating:* ${rating}
+🔮 *Title:* ${title}
+🪄 *Rating:* ${rating}
 
-🌍 *Description:*  
+📃 *Description:*  
 ${description}
 ──────────────────
 📥 *Downloading...*
+
+*╔⏤⏤⏤⏤⏤⏤╝ᎴᏁ ╚⏤⏤⏤⏤⏤⏤╗*
+
+⭕ *ΝᎬᏔՏ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAyHEQ7tkjI6v4ACw30
+⭕ *ᎷᏆႮՏᏆᏟ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029Vb3uqklAInPchBUjck3l
+⭕ *ᎷϴᏙᏆᎬ ႮᏢᎠᎪͲᎬ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAXPal4yltU1ep32l0H
+⭕ *ՏͲᏆᏦᎬᎡ ᏔϴᎡᏞᎠ ᏟᎻᎪΝΝᎬᏞ ➻*
+https://whatsapp.com/channel/0029Vb5xbu6JuyAAH9biN10H
+
+*╚⏤⏤⏤⏤⏤⏤╗ᎴᏁ ╔⏤⏤⏤⏤⏤⏤╝*
         `.trim();
   },
   ANIMOMOVIE: function (movieData) {
     return `
-🎬 *『 𝑨𝑵𝑱𝑼 Xᴾᴿᴼ 𝑪𝑰𝑵𝑬𝑴𝑨 』* 🍿  
+📽️ *『 𝐃𝐍 𝐌𝐎𝐕𝐈𝐄𝐒 』* 🍿  
         
-✨ ${movieData.title}  
-⭐ Description:${movieData.description} 
+🔮 ${movieData.title}  
+🪄 Description:${movieData.description} 
     
-🌍  ${movieData.plot} 
+🌍  ${movieData.plot}
+
+*╔⏤⏤⏤⏤⏤⏤╝ᎴᏁ ╚⏤⏤⏤⏤⏤⏤╗*
+
+⭕ *ΝᎬᏔՏ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAyHEQ7tkjI6v4ACw30
+⭕ *ᎷᏆႮՏᏆᏟ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029Vb3uqklAInPchBUjck3l
+⭕ *ᎷϴᏙᏆᎬ ႮᏢᎠᎪͲᎬ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAXPal4yltU1ep32l0H
+⭕ *ՏͲᏆᏦᎬᎡ ᏔϴᎡᏞᎠ ᏟᎻᎪΝΝᎬᏞ ➻*
+https://whatsapp.com/channel/0029Vb5xbu6JuyAAH9biN10H
+
+*╚⏤⏤⏤⏤⏤⏤╗ᎴᏁ ╔⏤⏤⏤⏤⏤⏤╝*
         `.trim();
   },
   FILMPOMOVIE: function (title, rating, year, duration) {
     return `
-🎬 *『 𝑨𝑵𝑱𝑼 Xᴾᴿᴼ 𝑪𝑰𝑵𝑬𝑴𝑨 』* 🍿  
+📽️ *『 𝐃𝐍 𝐌𝐎𝐕𝐈𝐄𝐒 』* 🍿  
 
-✨ *𝙼𝚘𝚟𝚒𝚎 𝙽𝚊𝚖𝚎:* ${title}
-⭐ *𝙸𝙼𝙳𝙱 𝚁𝚊𝚝𝚒𝚗𝚐:* ${rating}
+🔮 *𝙼𝚘𝚟𝚒𝚎 𝙽𝚊𝚖𝚎:* ${title}
+🪄 *𝙸𝙼𝙳𝙱 𝚁𝚊𝚝𝚒𝚗𝚐:* ${rating}
 📅 *𝚁𝚎𝚕𝚎𝚊𝚜𝚎 𝙳𝚊𝚝𝚎:* ${year}
-⏳ *𝙳𝚞𝚛𝚊𝚝𝚒𝚘𝚗:* ${duration}
+⏰ *𝙳𝚞𝚛𝚊𝚝𝚒𝚘𝚗:* ${duration}
+
+*╔⏤⏤⏤⏤⏤⏤╝ᎴᏁ ╚⏤⏤⏤⏤⏤⏤╗*
+
+⭕ *ΝᎬᏔՏ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAyHEQ7tkjI6v4ACw30
+⭕ *ᎷᏆႮՏᏆᏟ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029Vb3uqklAInPchBUjck3l
+⭕ *ᎷϴᏙᏆᎬ ႮᏢᎠᎪͲᎬ ᏟᎻᎪΝΝᎬᏞ*➻
+https://whatsapp.com/channel/0029VbAXPal4yltU1ep32l0H
+⭕ *ՏͲᏆᏦᎬᎡ ᏔϴᎡᏞᎠ ᏟᎻᎪΝΝᎬᏞ ➻*
+https://whatsapp.com/channel/0029Vb5xbu6JuyAAH9biN10H
+
+*╚⏤⏤⏤⏤⏤⏤╗ᎴᏁ ╔⏤⏤⏤⏤⏤⏤╝*
         `.trim();
   },
   //=====================youtube===========================================
   SONG: function (data) {
     return `
-🎵 SONG DOWNLOADER
+🎺 SONG DOWNLOADER
       
 ✅ Song Found!
           
-• 🎶 *Title:* ${data.title}
+• 🪗 *Title:* ${data.title}
 • ⏱️ *Duration:* ${data.timestamp}
 • 👁️ *Views:* ${data.views}
 • 📅 *Uploaded On:* ${data.ago}
-• 🔗 *Link:* ${data.url}
+• ⛓‍💥 *Link:* ${data.url}
           
-🎧 Enjoy your music with Queen Anju Bot  
-❤️ Created by Janith Rashmika
+🎧 Enjoy your music with 𝑫𝑰𝑳𝑨 Bot  
+❤️ Created by DILAKSHA PRABATH 
         `.trim();
   },
   YTMP3: function (data) {
     return `
-🎥 *MP3 Download Found!* 
+🎯 *MP3 Download Found!* 
 
 ➥ *Title:* ${data.title} 
 ➥ *Duration:* ${data.timestamp} 
@@ -276,28 +478,28 @@ ${description}
 ➥ *Uploaded On:* ${data.ago} 
 ➥ *Link:* ${data.url}
 
-🎬 *Enjoy the video brought to you by Queen Anju Bot!* 
+🦠 *Enjoy the video brought to you by DILA Bot!*  
         `.trim();
   },
   VIDEO: function (data) {
     return `
-🎵 VIDEO DOWNLOADER
+🎊 VIDEO DOWNLOADER
       
 ✅ VIDEO Found!
           
-• 🎶 *Title:* ${data.title}
-• ⏱️ *Duration:* ${data.timestamp}
+• 🏮 *Title:* ${data.title}
+• ⏰ *Duration:* ${data.timestamp}
 • 👁️ *Views:* ${data.views}
 • 📅 *Uploaded On:* ${data.ago}
-• 🔗 *Link:* ${data.url}
+• ⛓‍💥 *Link:* ${data.url}
           
-🎧 Enjoy your music with Queen Anju Bot  
-❤️ Created by Janith Rashmika
+🧧 Enjoy your music with 𝑫𝑰𝑳𝑨 Bot  
+❤️ Created by DILAKSHA PRABATH 
         `.trim();
   },
   YTMP4: function (data) {
     return `
-🎥 *MP4 Download Found!* 
+🧬 *MP4 Download Found!* 
 
 ➥ *Title:* ${data.title} 
 ➥ *Duration:* ${data.timestamp} 
@@ -305,13 +507,13 @@ ${description}
 ➥ *Uploaded On:* ${data.ago} 
 ➥ *Link:* ${data.url} 
 
-🎬 *Enjoy the video brought to you by Queen Anju Bot!* 
+🧲 *Enjoy the video brought to you by DILA🪄* 
         `.trim();
   },
   //=====================hiru news===========================================
   AUTONEWS: function (source, newsData, isGroup, groupMetadata, conf) {
     return `
-📰 *『 𝑨𝑵𝑱𝑼 Xᴾᴿᴼ 𝑵𝑬𝑾𝑺 』* 🗞️
+📰 *『 𝑫𝑵 𝑵𝑬𝑾𝑺 』* 🗞️
 
 ✨ *${source.toUpperCase()} News Update*
 
@@ -326,13 +528,13 @@ ${isGroup ? `\n\n📨 *Shared In:* ${groupMetadata?.subject || "Group"}\n🛡️
   },
   AUTOMOVIE: function (title, rating, metadata, description, isGroup, groupMetadata, conf) {
     return `
-🎬 *『 𝑨𝑵𝑱𝑼 Xᴾᴿᴼ 𝑪𝑰𝑵𝑬𝑴𝑨 』* 🍿
+📽️ *『 𝐃𝐍 𝐌𝐎𝐕𝐈𝐄𝐒 』* 🍿
 
-✨ *𝙼𝚘𝚟𝚒𝚎 𝙽𝚊𝚖𝚎:* ${title}
-⭐ *𝙸𝙼𝙳𝙱 𝚁𝚊𝚝𝚒𝚗𝚐:* ${rating?.value || "𝙽/𝙰"} ★
+🔮 *𝙼𝚘𝚟𝚒𝚎 𝙽𝚊𝚖𝚎:* ${title}
+🪄 *𝙸𝙼𝙳𝙱 𝚁𝚊𝚝𝚒𝚗𝚐:* ${rating?.value || "𝙽/𝙰"} ★
 📆 *𝚁𝚎𝚕𝚎𝚊𝚜𝚎 𝙳𝚊𝚝𝚎:* ${metadata?.releaseDate || "𝙽/𝙰"}
-🌐 *𝙲𝚘𝚞𝚗𝚝𝚛𝚢:* ${metadata?.country || "𝙽/𝙰"}
-⏱️ *𝙳𝚞𝚛𝚊𝚝𝚒𝚘𝚗:* ${metadata?.runtime || "𝙽/𝙰"}
+🌍 *𝙲𝚘𝚞𝚗𝚝𝚛𝚢:* ${metadata?.country || "𝙽/𝙰"}
+⏰ *𝙳𝚞𝚛𝚊𝚝𝚒𝚘𝚗:* ${metadata?.runtime || "𝙽/𝙰"}
 🎭 *𝙶𝚎𝚗𝚛𝚎𝚜:* ${metadata?.genres?.map(g => `#${g}`).join(" • ") || "𝙽/𝙰"}
 
 ${isGroup ? `📨 *𝙎𝙝𝙖𝙧𝙚𝙙 𝙄𝙣:* ${groupMetadata?.subject || "Group"}\n🛡️ *𝘼𝙙𝙢𝙞𝙣:* ${conf.MNAME}` : ""}
@@ -345,10 +547,10 @@ ${conf.FOOTER}`.trim();
   },
   AUTOSONG: function (foundSong, currentArtist, nextArtist, config, botNumber, isGroup, groupMetadata) {
     return `
-✨ *『 𝑨𝑵𝑱𝑼 Xᴾᴿᴼ 𝑴𝑼𝑺𝑰𝑪 』* 🎧
+💃 *『 𝐃𝐍  𝑴𝑰𝑼𝑺𝑰𝑪 』* 🥁
 
 ━━━━━━━━━━━━━━━━━━━
-🎶 *𝚂𝙾𝙽𝙶 𝙳𝙴𝚃𝙰𝙸𝙻𝚂* 🎼
+🥁 *𝚂𝙾𝙽𝙶 𝙳𝙴𝚃𝙰𝙸𝙻𝚂* 🎺
 ╠➤ *𝚃𝚒𝚝𝚕𝚎:* ${foundSong.title}
 ╠➤ *𝙰𝚛𝚝𝚒𝚜𝚝:* ${currentArtist.replace(" original", "")}
 ╠➤ *𝙳𝚞𝚛𝚊𝚝𝚒𝚘𝚗:* ${foundSong.timestamp || "𝙽/𝙰"}
@@ -356,22 +558,22 @@ ${conf.FOOTER}`.trim();
 ╠➤ *𝚄𝚙𝚕𝚘𝚊𝚍𝚎𝚍:* ${foundSong.ago || "𝙽/𝙰"}
 ╠➤ *𝙽𝚎𝚡𝚝 𝙰𝚛𝚝𝚒𝚜𝚝:* ${nextArtist}
 ━━━━━━━━━━━━━━━━━━━
-📌 *𝚈𝚘𝚞𝚃𝚞𝚋𝚎 𝙻𝙸𝙽𝙺:* 
+🪂 *𝚈𝚘𝚞𝚃𝚞𝚋𝚎 𝙻𝙸𝙽𝙺:* 
     ${foundSong.url}
     ${isGroup ? `
 
 ━━━━━━━━━━━━━━━━━━━
-👥 *𝙶𝚁𝙾𝚄𝙿 𝙸𝙽𝙵𝙾* 
+💃 *𝙶𝚁𝙾𝚄𝙿 𝙸𝙽𝙵𝙾* 
 ╠➤ *𝙽𝚊𝚖𝚎:* ${groupMetadata?.subject || "Group"}
 ╠➤ *𝙰𝚍𝚖𝚒𝚗:* ${config.MNAME}
 ╠➤ *𝙱𝚘𝚝 𝙽𝚞𝚖𝚋𝚎𝚛:* ${botNumber}
 ━━━━━━━━━━━━━━━━━━━
 ` : ""}
-${config.FOOTER || "🎼 Enjoy the music! 🎧"}`.trim();
+${config.FOOTER || "*✦͙͙͙*͙*❥⃝∗⁎.ʚ𝙳𝙽 𝚖𝚒𝚞𝚜𝚒𝚌 ɞ.⁎∗❥⃝**͙✦͙͙͙*"}`.trim();
   },
   AIMODEPROMPT: function (userMessage) {
     return `
-    You're an advanced AI assistant called "King Rashmika AI." You're professional, respectful, and knowledgeable, always ready to assist with expertise. 👑 Your goal is to provide helpful, accurate, and engaging responses while maintaining a courteous and professional tone.
+    You're an advanced AI assistant called "King DILA AI."  
     
     usermessage in {${userMessage}}`;
   }
